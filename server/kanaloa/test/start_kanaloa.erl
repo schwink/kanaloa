e229aeec3278f76df664959b4fdcd46b350e7529
Stephen Schwink <stephen.schwink@gmail.com>
2010-08-05 02:51:36 -0700

    Beefing up the test/sample application with the more comprehensive convention used by thruster.

--- server/kanaloa/test/start_kanaloa.erl
+++ server/kanaloa/test/start_kanaloa.erl
@@ -1,38 +1,13 @@
 %% @author Stephen Schwink <kanaloa@schwink.net>
 %% @copyright 2010 Stephen Schwink.
 
-%% @doc TEMPLATE.
+%% @doc Test kanaloa application.
 
 -module(start_kanaloa).
 -author('Stephen Schwink <kanaloa@schwink.net>').
 -export([start/0]).
 
-handle_connection(orphaned) ->
-    receive
-	{connection, NewConnection} ->
-	    NewConnection:log("Handler is un-orphaned.", []),
-	    handle_connection(NewConnection)
-    after 60000 ->
-	    io:format("Didn't get a new connection in time, the handler dies.\n", []),
-	    ok
-    end;
-handle_connection(Connection) ->
-    receive
-	{connection, NewConnection} ->
-	    NewConnection:log("Handler replacing live connection.", []),
-	    Connection:close(),
-	    handle_connection(NewConnection);
-	{'EXIT', _Owner, _Reason} ->
-	    Connection:log("Handler orphaned.", []),
-	    handle_connection(orphaned);
-	{chunk, Data} ->
-	    Connection:log("Handler got a chunk! '~w'", [Data]),
-	    Reply = <<"Got your message, which is re-encoded as:">>,
-	    BodyText = mochijson2:encode(Data),
-	    BodyBinary = iolist_to_binary(BodyText),
-	    Connection:send(<<Reply/binary, BodyBinary/binary>>),
-	    handle_connection(Connection)
-    end.
+-define(CONNECTION_ORPHAN_TIMEOUT, 60000).
 
 %% @spec start() -> ok
 %% @doc Start the kanaloa mochiweb server.
@@ -49,4 +24,62 @@ start() ->
     receive
 	exit -> % Apparently this function, passed to erl with "-s", isn't supposed to return.
 	    ok
+    end.
+
+%% @doc Control comes here when we want to receive the next message for a presumably healty connection.
+handle_connection(Connection) ->
+    receive
+	% First, check for high-priority messages that indicate a change of connection state.
+	% We need to handle these first to prevent sending data to a dead response.
+	
+	{connection, NewConnection} ->
+	    NewConnection:log("Handler replacing live connection.", []),
+	    new_connection(Connection, NewConnection);
+	
+	{'EXIT', _Owner, _Reason} ->
+	    handle_orphaned(Connection)
+    
+    after 0 ->
+	  % Next, process regular application messages.
+	  receive_normal(Connection)
+    end.
+
+%% @doc Control comes here while we wait for the client to reconnect.
+handle_orphaned(OldConnection) ->
+    OldConnection:log("Handler orphaned.", []),
+    
+    receive
+	{connection, NewConnection} ->
+	    NewConnection:log("Handler is un-orphaned.", []),
+	    new_connection(OldConnection, NewConnection)
+    
+    after ?CONNECTION_ORPHAN_TIMEOUT ->
+	    OldConnection:log("Didn't get a new connection in time, the handler dies.", []),
+	    orphaned
+    end.
+
+new_connection(OldConnection, NewConnection) ->
+    catch OldConnection:close(),
+    handle_connection(NewConnection).
+
+receive_normal(Connection) ->
+    receive
+	% Continue to listen for the high-priority events, even if they weren't in the queue initially.
+	{connection, NewConnection} ->
+	    NewConnection:log("Handler replacing live connection.", []),
+	    new_connection(Connection, NewConnection);
+	{'EXIT', _Owner, _Reason} ->
+	    handle_orphaned(Connection);
+
+	% React to messages from Kanaloa and from your application.
+	{chunk, Data} ->
+	    Connection:log("Handler got a chunk! '~w'", [Data]),
+	    
+	    Reply = <<"Got your message, which is re-encoded as:">>,
+	    BodyText = mochijson2:encode(Data),
+	    BodyBinary = iolist_to_binary(BodyText),
+	    Connection:send(<<Reply/binary, BodyBinary/binary>>),
+	    
+	    handle_connection(Connection)
+    
     end.
