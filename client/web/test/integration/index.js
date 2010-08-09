62f81ccec945ae1b035da07a994e23b491fb5ecf
Stephen Schwink <stephen.schwink@gmail.com>
2010-08-08 18:52:04 -0700

    Adds simple send/onDataReceived integration test.

--- client/web/test/integration/index.js
+++ client/web/test/integration/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	
+	var server = "/kanaloa/testsvc/";
+	
 	asyncTest("basic connect and disconnect", 4, function() {
-		var connection = new KanaloaConnection("/kanaloa/testsvc/");
+		var connection = new KanaloaConnection(server);
 		
 		connection.onConnectionOpened = function() {
 		    ok( true, "onConnectionOpened callback received" );
@@ -18,5 +20,19 @@ $(document).ready(function(){
 		};
 		
 		connection.connect();
+	    });
+	
+	asyncTest("basic message send and receive", 1, function() {
+		var message = "test message " + Date.UTC();
+
+		var connection = new KanaloaConnection(server);
+
+		connection.onDataReceived = function(data) {
+		    equals(message, data.message, "received valid message");
+		    
+		    start();
+		};
+		
+		connection.send(message);
 	    });
     });
