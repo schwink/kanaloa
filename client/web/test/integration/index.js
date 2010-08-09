dc79afabc2b147c8a3e3e687689b1b395f38482f
Stephen Schwink <stephen.schwink@gmail.com>
2010-08-08 19:09:50 -0700

    Adds a "stress" test, which sends 20 messages and confirms that they are processed in order.

--- client/web/test/integration/index.js
+++ client/web/test/integration/index.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	
 	var server = "/kanaloa/testsvc/";
 	
+	module("Basics");
+	
 	asyncTest("basic connect and disconnect", 4, function() {
 		var connection = new KanaloaConnection(server);
 		
@@ -29,10 +31,36 @@ $(document).ready(function(){
 
 		connection.onDataReceived = function(data) {
 		    equals(message, data.message, "received valid message");
-		    
+
+		    connection.disconnect();
 		    start();
 		};
 		
 		connection.send(message);
 	    });
+	
+	module("Stress");
+	
+	var repetitions = 20;
+	asyncTest("message order preservation", repetitions + 1, function() {
+		var receivedCount = 0;
+		
+		var connection = new KanaloaConnection(server);
+		
+		connection.onDataReceived = function(data) {
+		    equals(receivedCount, data.message, "received message " + receivedCount + " in order");
+		    receivedCount++;
+		    
+		    if (receivedCount == repetitions) {
+			connection.disconnect();
+			ok(true, "received all messages");
+			start();
+		    }
+		}
+		
+		for (var s = 0; s < repetitions; s++) {
+		    connection.send(s);
+		}
+	    });
+	
     });
